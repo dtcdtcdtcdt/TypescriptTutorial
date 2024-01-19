1e45108ff6070dc6669031714ad998c603ae0ba8
Daniel Torres Chico SQA <danieltorreschicosqa@Daniels-MacBook-Pro.local>
2024-01-18 23:48:08 -0800

    Arrays

---
+++ myArray.ts
@@ -0,0 +1,17 @@
+const superHeroes: string[] = []
+//const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type User3 = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User3[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+allUsers.push({name: "", isActive: false})
