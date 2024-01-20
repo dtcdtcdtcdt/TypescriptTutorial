0ffd29407596e718a45730992830a55de012206f
Daniel Torres Chico SQA <danieltorreschicosqa@Daniels-MacBook-Pro.local>
2024-01-20 14:45:26 -0800

    Enums

---
+++ myEnums.ts
@@ -0,0 +1,13 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    FOURTH = 0
+}
+
+let seat: number = 0;
+
